3f70d1a32afdd9d437a330f7dd7d6e7c2bedfc0c
Ben Mohammad <benmohammad593@gmail.com>
2020-08-21 09:13:36 +0100

    ActivityUtils.java added...

---
+++ app/src/main/java/com/benmohammad/javatodomvi/util/ActivityUtils.java
@@ -0,0 +1,22 @@
+package com.benmohammad.javatodomvi.util;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.fragment.app.FragmentManager;
+import androidx.fragment.app.FragmentTransaction;
+
+import static com.google.common.base.Preconditions.checkNotNull;
+
+
+public class ActivityUtils {
+
+    public static void addFragmentToActivity(@NonNull FragmentManager fm,
+                                             @NonNull Fragment fragment,
+                                             int frameId) {
+        checkNotNull(fm);
+        checkNotNull(fragment);
+        FragmentTransaction transaction = fm.beginTransaction();
+        transaction.add(frameId, fragment);
+        transaction.commit();
+    }
+}
